1555435a400b9ff053296a81506e431139838a6d
Mattia <mattia.nicolis04@gmail.com>
2026-01-10 12:03:38 +0100

    update server (sha-256 calculation corrected)

--- src/server/server.c
+++ src/server/server.c
@@ -5,9 +5,38 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <pthread.h>
+#include<openssl/sha.h>
 
 #include "server.h"
 #include "../common/protocol.h"
+
+void digstfile(const char *filename, unsigned char *hash) {
+    SHA256_CTX ctx;
+    SHA256_Init(&ctx);
+
+    char buffer[32];
+    int fd = open(filename, O_RDONLY);
+    if(fd < 0) {
+        perror("open");
+        return;
+    }
+
+    ssize_t n;
+    while((n = read(fd, buffer, sizeof(buffer))) > 0) {
+        SHA256_Update(&ctx, buffer, n);
+    }
+
+    SHA256_Final(hash, &ctx);
+    close(fd);
+}
+
+void hash_to_string(unsigned char *hash, char *output) {
+    for(int i=0; i< 32; i++) {
+        sprintf(output + (i*2), "%02x", hash[i]);
+    }
+
+    output[64] = '\0';
+}
 
 void* handle_request(void* arg) {
     request_t* req = (request_t*)arg;
@@ -17,7 +46,10 @@ void* handle_request(void* arg) {
 
     response_t res;
     res.client_pid = req->client_pid;
-    strcpy(res.hash, "TODO");
+    
+    unsigned char hash[32];
+    digstfile(req->filepath, hash);
+    hash_to_string(hash, res.hash);
 
     int fd_res = open(FIFO_RES, O_WRONLY);
     write(fd_res, &res, sizeof(res));
